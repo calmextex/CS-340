d723a40e3c4587cbdeab2124433b6d51662ccbba
Mikaella Shelby <83800814+brwerx@users.noreply.github.com>
2024-05-09 19:17:31 -0700

    Update DDL.sql - comments & varchar

--- DDL.sql
+++ DDL.sql
@@ -1,48 +1,51 @@
+--Team 59 : Mikaella Shelby & Abraham Zamora 
+--Project Title: Student Information System
+
 SET FOREIGN_KEY_CHECKS = 0;
 SET AUTOCOMMIT = 0;
 
--- Creating the Students table
+--Students Table: Demographic data for students. There will be one record for each student.
 DROP TABLE IF EXISTS Students;
 CREATE TABLE Students (
     studentID int NOT NULL AUTO_INCREMENT,
-    firstName varchar(255) NOT NULL,
-    lastName varchar(255) NOT NULL,
+    firstName varchar(25) NOT NULL,
+    lastName varchar(25) NOT NULL,
     dob date NOT NULL,
     gradeLevel int NOT NULL,
     entryDate date NOT NULL,
     leaveDate date,
-    email varchar(255) NOT NULL,
-    phoneNumber varchar(255),
-    address varchar(255),
-    city varchar(255),
-    state varchar(255),
-    zipCode varchar(255),
-    parentGuardian varchar(255),
+    email varchar(50) NOT NULL,
+    phoneNumber varchar(12),
+    address varchar(50),
+    city varchar(25),
+    state varchar(2),
+    zipCode varchar(10),
+    parentGuardian varchar(50),
     PRIMARY KEY (studentID),
     UNIQUE KEY (studentID)
 );
 
--- Creating the StateCourseCodes table
+--StateCourseCodes Table: State course codes help to identify the core material being taught.
 DROP TABLE IF EXISTS StateCourseCodes;
 CREATE TABLE StateCourseCodes (
     stateCourseCode int NOT NULL,
-    subject varchar(255) NOT NULL,
+    subject varchar(25) NOT NULL,
     PRIMARY KEY (stateCourseCode),
     UNIQUE KEY (stateCourseCode)
 );
 
--- Creating the Courses table
+--Course Table: Course Information. This will be a record for a specific course.
 DROP TABLE IF EXISTS Courses;
 CREATE TABLE Courses (
     courseID int NOT NULL AUTO_INCREMENT,
-    courseName varchar(255) NOT NULL,
+    courseName varchar(50) NOT NULL,
     stateCourseCode int NOT NULL,
     PRIMARY KEY (courseID),
     UNIQUE KEY (courseID),
     FOREIGN KEY (stateCourseCode) REFERENCES StateCourseCodes(stateCourseCode) ON UPDATE CASCADE ON DELETE CASCADE ON DELETE SET NULL
 );
 
--- Creating the Enrollments table
+--Enrollments Table: This is will keep track of enrollment term by term.
 DROP TABLE IF EXISTS Enrollments;
 CREATE TABLE Enrollments (
     enrollmentID int NOT NULL AUTO_INCREMENT,
@@ -54,7 +57,7 @@ CREATE TABLE Enrollments (
     FOREIGN KEY (studentID) references Students(studentID) ON UPDATE CASCADE ON DELETE CASCADE ON DELETE SET NULL
 );
 
--- Creating the CourseEnrollments table
+--CourseEnrollments: Tracks student enrollment in courses. A student can be assigned multiple courses at once, including completed courses.
 DROP TABLE IF exists CourseEnrollments;
 CREATE TABLE CourseEnrollments (
     enrollmentID int NOT NULL,
@@ -65,12 +68,12 @@ CREATE TABLE CourseEnrollments (
     CONSTRAINT FOREIGN KEY (enrollmentID) REFERENCES Enrollments(enrollmentID) ON UPDATE CASCADE
 );
 
--- Creating the Grades table
+--Grades Table: Grades assigned for courses completed. Once a course is complete, grades are assigned.
 DROP TABLE IF EXISTS Grades;
 CREATE TABLE Grades (
     courseID int NOT NULL,
     studentID int NOT NULL,
-    gradeAssigned varchar(255) NOT NULL,
+    gradeAssigned varchar(2) NOT NULL,
     CONSTRAINT FOREIGN KEY (courseID) REFERENCES Courses(courseID) ON DELETE CASCADE,
     CONSTRAINT FOREIGN KEY (studentID) REFERENCES Students(studentID) ON DELETE CASCADE
 );
@@ -218,15 +221,67 @@ INSERT INTO CourseEnrollments (
     courseStartDate,
     courseEndDate
 )
-VALUES(1, 1, '2023-07-03', '2023-12-15'),(5, 1, '2024-01-05', NULL),(2, 2, '2023-07-03', '2024-04-21'),(3, 3, '2023-07-03', NULL),(4, 3, '2023-07-03', '2024-01-30'),(5,3,'2024-01-05', NULL);
+VALUES
+(
+    1, 
+    1, 
+    '2023-07-03', 
+    '2023-12-15'
+),
+(
+    5, 
+    1, 
+    '2024-01-05', 
+    NULL
+),
+(
+    2, 
+    2, 
+    '2023-07-03', 
+    '2024-04-21'
+),
+(
+    3, 
+    3, 
+    '2023-07-03', 
+    NULL
+),
+(
+    4, 
+    3, 
+    '2023-07-03', 
+    '2024-01-30'
+),
+(
+    5,
+    3,
+    '2024-01-05', 
+    NULL
+);
 --SELECT * FROM CourseEnrollments;
+
 -- insert sample data into the grades table
 INSERT INTO Grades (
     courseID,
     studentID,
     gradeAssigned
 )
-VALUES(1,1,'A'), (2,2,'B+'), (4,3,'A-');
+VALUES
+(
+    1,
+    1,
+    'A'
+), 
+(
+    2,
+    2,
+    'B+'
+), 
+(
+    4,
+    3,
+    'A-'
+);
 --SELECT * FROM Grades;
 
 -- Enable foreign key checks and commit changes
